9be3a21588daad14e259374ca82fd0c4e7ad3c2b
busra3442 <162158913+busra3442@users.noreply.github.com>
2024-03-24 01:38:21 +0300

    Update piyango.java

--- piyango.java
+++ piyango.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class piyango {
-    public class calisma13 {
         public static void main(String[] args) {
             Scanner scn = new Scanner(System.in);
             int sansliSayi = (int) (Math.random()*99+1);
@@ -32,4 +31,3 @@ public class piyango {
             }
         }
     }
-}
